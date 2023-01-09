0cc00213e092b3adc1b2e0f97cdd78ebb1b81e94
jsr09 <97914865+jsr09@users.noreply.github.com>
2023-01-09 12:50:14 -0500

    Update 1

--- dataBase.js
+++ dataBase.js
@@ -1,1 +1,28 @@
+export const housingData = [
+    // {
+    //     Type: '',
+    //     Address: '',
+    //     City: '',
+    //     State: '',  
+    // },
+    {
+        Type: 'House',
+        Address: '1234 fake place',
+        City: 'Oakland',
+        State: 'CA',  
+    },
+    {
+        Type: 'Apartment',
+        Address: '8543 another place, Unit 2',
+        City: 'lansing',
+        State: 'MI',  
+    },
+    {
+        Type: 'Duplex',
+        Address: '9876 rental ave',
+        City: 'Chicago',
+        State: 'IL',  
+    }
 
+
+]
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import {housingData} from './dataBase.js'
+// console.log(housingData)
 
+
+class HousingDetails {
+    constructor(type, address, city, state){
+        
+
+    }
+}
